835cad9754755bf1a6de036f2b313f2b7ad45dff
Clevin <Clevin.pro@gmail.com>
2019-12-20 15:06:19 +0200

    logOut :D

--- src/controller/routes/auth/logout.js
+++ src/controller/routes/auth/logout.js
@@ -1,7 +1,8 @@
 const logout = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
-  response.write('<h1>logout!</h1>');
-  response.end();
+  response.status(200);
+  response.json({
+    status: 'success',
+  });
 };
 
 module.exports = logout;
